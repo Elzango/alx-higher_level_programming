9856120d98c74b639cfeff8e02cdc287e6c8ebf7
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2023-03-07 23:03:21 +0100

    Add  scripts/programs to the project 0x01_python_if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 10-add.py
+# Mahmud Zango <elzango.mz@gmail.com>
+
+
+def add(a, b):
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Mahmud Zango 
+
+def pow(a, b):
+    return (a ** b)
