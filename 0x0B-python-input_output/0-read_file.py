506da61b85a0f2afa699b34c80cdbd08aca3262e
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2023-04-12 17:51:24 +0100

    Fix some bugs on some scripts

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,9 +2,8 @@
 # 0-read_file.py
 # Arth: Mahmud
 """Defines a text file-reading function."""
-
-
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
+
